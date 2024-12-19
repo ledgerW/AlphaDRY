7b68ebf5807676997e675e97c158602b35db1a15
Ledger West <ledger.west@gmail.com>
2024-12-19 20:59:23 +0000

    fix: deploy

--- main.py
+++ main.py
@@ -38,5 +38,5 @@ async def read_index(request: Request):
 
 if __name__ == "__main__":
     # Use port 80 for deployment or 2024 as fallback
-    port = int(os.getenv("PORT", 8000))
+    port = int(os.environ.get('PORT', 80))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
