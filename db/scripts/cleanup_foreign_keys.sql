c60a593dc9f540d1c2ae60258ae20ad066d33c7d
Ledger West <ledger.west@gmail.com>
2024-12-30 20:23:10 +0000

    fix

---
+++ db/scripts/cleanup_foreign_keys.sql
@@ -0,0 +1,52 @@
+-- Cleanup dangling foreign key references
+DO $$
+DECLARE
+    table_name text;
+    column_name text;
+    referenced_table text;
+    referenced_column text;
+    cleanup_sql text;
+BEGIN
+    -- Get all foreign key constraints in the database
+    FOR table_name, column_name, referenced_table, referenced_column IN
+        SELECT
+            tc.table_name,
+            kcu.column_name,
+            ccu.table_name AS referenced_table,
+            ccu.column_name AS referenced_column
+        FROM information_schema.table_constraints tc
+        JOIN information_schema.key_column_usage kcu
+            ON tc.constraint_name = kcu.constraint_name
+            AND tc.table_schema = kcu.table_schema
+        JOIN information_schema.constraint_column_usage ccu
+            ON ccu.constraint_name = tc.constraint_name
+            AND ccu.table_schema = tc.table_schema
+        WHERE tc.constraint_type = 'FOREIGN KEY'
+        AND tc.table_schema = 'public'
+    LOOP
+        -- Build and execute cleanup SQL for each foreign key
+        cleanup_sql := format('
+            DELETE FROM %I
+            WHERE %I IN (
+                SELECT %I
+                FROM %I t1
+                LEFT JOIN %I t2 ON t1.%I = t2.%I
+                WHERE t2.%I IS NULL
+            )',
+            table_name,
+            column_name,
+            column_name,
+            table_name,
+            referenced_table,
+            column_name,
+            referenced_column,
+            referenced_column
+        );
+        
+        EXECUTE cleanup_sql;
+        
+        RAISE NOTICE 'Cleaned up dangling references in %.% referencing %.%',
+            table_name, column_name, referenced_table, referenced_column;
+    END LOOP;
+END
+$$;
